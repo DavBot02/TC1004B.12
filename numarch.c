4876eaf1110cc4fc6528022aeadbb568b9bda925
Luis David López Magaña <davidlopezsth@gmail.com>
2021-10-11 12:18:49 +0000

    pipe and numarch

---
+++ numarch.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main() {
+    int fd[2];
+    pipe(fd);
+    int pid = fork();
+    if (pid == 0) {
+        // close(0);
+
+        dup2(1, fd[1]);
+        execlp("wc", "wc", "-l", NULL);
+    }
+    else {
+        // close(1);
+        
+        dup2(0, fd[0]);
+        execlp("ls", "ls", NULL);
+    }
+
+    return 0;
+}
